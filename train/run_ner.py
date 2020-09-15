1b7325cd2397e058f3c1c4721a36a6e995608fa8
Yu-Lun Chiang <chiangyulun0914@gmail.com>
2020-09-15 13:20:35 +0800

    init run_ner

---
+++ train/run_ner.py
@@ -0,0 +1,173 @@
+# coding=utf-8
+# Author: Allen.Chiang
+# Description: run NER task either as sequence labeling task or as machine reading comprehension
+
+import logging
+import logging.config
+import os
+import sys
+from typing import List, Dict
+from dataclasses import dataclass, field
+from transformers import (
+    BertConfig,
+    BertTokenizer,
+    BertForTokenClassification,
+    HfArgumentParser,
+    TrainingArguments,
+    Trainer,
+    set_seed
+)
+from torch.utils.tensorboard import SummaryWriter
+from configs.args_dataclass import DataTrainingArguments, ModelArguments
+from utils.sl import NerAsSLDataset, get_labels, align_predictions, compute_metrics, write_predictions_to_file
+
+logging.config.fileConfig('configs/logging.conf')
+logger = logging.getLogger(__name__)
+
+
+def main():
+    
+    #--- Parse args ---#
+    parser = HfArgumentParser((DataTrainingArguments, ModelArguments, TrainingArguments))
+    data_args, model_args, training_args = parser.parse_json_file(json_file=sys.argv[1])  #pylint: disable=unbalanced-tuple-unpacking
+    logger.debug(f"data_args: {data_args}")
+    logger.debug(f"model_args: {model_args}")
+    logger.debug(f"training_args: {training_args}")
+
+    if model_args.task not in ["sl", "mrc"]:
+        raise ValueError("task in configs/config.json must be either sl or mrc.")
+
+    logger.warning(
+        "Process rank: %s, device: %s, n_gpu: %s, distributed training: %s, 16-bits training: %s",
+        training_args.local_rank,
+        training_args.device,
+        training_args.n_gpu,
+        bool(training_args.local_rank != -1),
+        training_args.fp16,
+    )
+    set_seed(training_args.seed)
+
+    if model_args.task == "sl":
+
+        #--- Prepare labels ---#
+        labels = get_labels(data_args.labels_path)
+        label_map: Dict[int, str] = {i: label for i, label in enumerate(labels)}
+        num_labels = len(labels)
+
+        #--- Prepare config, tokenizer, model ---#
+        config = BertConfig.from_pretrained(
+            model_args.config_name if model_args.config_name else model_args.model_name_or_path,
+            num_labels=num_labels,
+            id2label=label_map,
+            label2id={label: i for i, label in enumerate(labels)},
+            cache_dir=model_args.cache_dir,
+        )
+        tokenizer = BertTokenizer.from_pretrained(
+            model_args.tokenizer_name if model_args.tokenizer_name else model_args.model_name_or_path,
+            cache_dir=model_args.cache_dir,
+            use_fast=model_args.use_fast,
+        )
+        model = BertForTokenClassification.from_pretrained(
+            model_args.model_name_or_path,
+            from_tf=bool(".ckpt" in model_args.model_name_or_path),
+            config=config,
+            cache_dir=model_args.cache_dir,
+        )
+
+        #--- Add tokens that are might not in vocab.txt ---#
+        add_tokens = ['瘜', '皰', '搐', '齲', '蛀', '髕', '闌', '疝', '嚥', '簍', '廔', '顳', '溼', '髖', '膈', '搔', '攣', '仟', '鐙', '蹠', '橈']
+        tokenizer.add_tokens(add_tokens)
+        model.resize_token_embeddings(len(tokenizer))
+
+        #--- Prepare datasets ---#
+        train_dataset = (
+            NerAsSLDataset(
+                data_dir=data_args.data_dir,
+                filename=data_args.train_filename,
+                tokenizer=tokenizer,
+                labels=labels,
+                model_type=config.model_type,
+                max_seq_length=data_args.max_seq_length,
+                overwrite_cache=data_args.overwrite_cache,
+            )
+            if training_args.do_train or training_args.do_eval
+            else None
+        )
+        eval_dataset = (
+            NerAsSLDataset(
+                data_dir=data_args.data_dir,
+                filename=data_args.dev_filename,
+                tokenizer=tokenizer,
+                labels=labels,
+                model_type=config.model_type,
+                max_seq_length=data_args.max_seq_length,
+                overwrite_cache=data_args.overwrite_cache,
+            )
+            if training_args.do_train or training_args.do_eval
+            else None
+        )
+        test_dataset = (
+            NerAsSLDataset(
+                data_dir=data_args.data_dir,
+                filename=data_args.test_filename,
+                tokenizer=tokenizer,
+                labels=labels,
+                model_type=config.model_type,
+                max_seq_length=data_args.max_seq_length,
+                overwrite_cache=data_args.overwrite_cache,
+            )
+            if training_args.do_predict
+            else None
+        )
+
+        #--- Initialize trainer from huggingface ---#
+        trainer = Trainer(
+            model=model,
+            args=training_args,
+            train_dataset=train_dataset,
+            eval_dataset=eval_dataset,
+            compute_metrics=compute_metrics,
+            tb_writer=SummaryWriter(training_args.logging_dir),
+            # optimizers
+        )
+
+        #--- Train ---#
+        if training_args.do_train:
+            trainer.train()
+            trainer.save_model()
+            if trainer.is_world_master():
+                tokenizer.save_pretrained(training_args.output_dir)
+        
+        #--- Evaluate all set ---#
+        if training_args.do_eval:
+            for dataset in [train_dataset, eval_dataset, test_dataset]:
+                metrics = trainer.evaluate(dataset)
+                evaluation_results_file = os.path.join(training_args.output_dir, "evaluation_results.txt")
+                if trainer.is_world_master():
+                    with open(evaluation_results_file, "w") as writer:
+                        logger.info(f"Eval results of {dataset.set}")
+                        writer.write(f"Eval results of {dataset.set}")
+                        for key, value in metrics.items():
+                            logger.info("  %s = %s", key, value)
+                            writer.write("%s = %s\n" % (key, value))
+        
+        #--- Predict test set ---#
+        if training_args.do_predict:
+            predictions, label_ids, metrics = trainer.predict(test_dataset)
+            preds_list, _ = align_predictions(predictions, label_ids, label_map)
+            test_predictions_file = os.path.join(training_args.output_dir, "test_predictions.txt")
+            if trainer.is_world_master():
+                with open(test_predictions_file, "w") as writer:
+                    with open(os.path.join(data_args.data_dir, data_args.test_filename), "r") as f:
+                        write_predictions_to_file(writer, f, preds_list)
+
+if __name__ == "__main__":
+    main()
+    
+
+    
+    
+    
+
+
+    
