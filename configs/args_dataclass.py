2b32f2da91dd632b08874f72a043300be6c110b4
Yu-Lun Chiang <chiangyulun0914@gmail.com>
2020-10-05 15:12:03 +0800

    add mrc args

--- configs/args_dataclass.py
+++ configs/args_dataclass.py
@@ -1,17 +1,19 @@
 # encoding=utf-8
 # Author: Allen.Chiang
-# Description: argsd of sequence labeling
+# Description: Definition of custom arguments
 
 from typing import Optional
 from dataclasses import dataclass, field
 
+
 @dataclass
-class DataTrainingArguments:
+class DataArguments:
     """
     Arguments pertaining to what data we are going to input our model for training and eval.
     """
     data_dir: str = field(
-        metadata={"help": "The input data dir. Should contain the train, dev, and test set."}
+        metadata={
+            "help": "The input data dir. Should contain the train, dev, and test set."}
     )
     labels_path: str = field(
         metadata={"help": "Path to a file containing all labels."}
@@ -28,6 +30,14 @@ class DataTrainingArguments:
         default="test.txt",
         metadata={"help": "The filename of test set."}
     )
+    overwrite_cache: bool = field(
+        default=False,
+        metadata={"help": "Overwrite the cached training and evaluation sets"}
+    )
+
+
+@dataclass
+class SLDataArguments:
     max_seq_length: int = field(
         default=128,
         metadata={
@@ -35,37 +45,82 @@ class DataTrainingArguments:
             "than this will be truncated, sequences shorter will be padded."
         }
     )
-    overwrite_cache: bool = field(
-        default=False, 
-        metadata={"help": "Overwrite the cached training and evaluation sets"}
+
+
+@dataclass
+class MRCDataArguments:
+    max_seq_length: int = field(
+        default=128,
+        metadata={
+            "help": "The maximum total input sequence length after tokenization. Sequences longer "
+            "than this will be truncated, sequences shorter will be padded."
+        }
     )
+    doc_stride: int = field(
+        default=128,
+        metadata={
+            "help": "When splitting up a long document into chunks, how much stride to take between chunks."},
+    )
+    max_query_length: int = field(
+        default=64,
+        metadata={
+            "help": "The maximum number of tokens for the question. Questions longer than this will "
+            "be truncated to this length."
+        },
+    )
+    max_answer_length: int = field(
+        default=30,
+        metadata={
+            "help": "The maximum length of an answer that can be generated. This is needed because the start "
+            "and end predictions are not conditioned on one another."
+        },
+    )
+    version_2_with_negative: bool = field(
+        default=False, metadata={"help": "If true, the SQuAD examples contain some that do not have an answer."}
+    )
+    null_score_diff_threshold: float = field(
+        default=0.0, metadata={"help": "If null_score - best_non_null is greater than the threshold predict null."}
+    )
+    n_best_size: int = field(
+        default=20, metadata={"help": "If null_score - best_non_null is greater than the threshold predict null."}
+    )
+    lang_id: int = field(
+        default=0,
+        metadata={
+            "help": "language id of input for language-specific xlm models (see tokenization_xlm.PRETRAINED_INIT_CONFIGURATION)"
+        },
+    )
+    threads: int = field(default=1, metadata={
+                         "help": "multiple threads for converting example to features"})
+
 
 @dataclass
 class ModelArguments:
     """
     Arguments pertaining to which model/config/tokenizer we are going to fine-tune from.
     """
-    task: str = field(
-        metadata={"help": "It takes NER either as sl (sequence labeling) or mrc (machine reading comprehension)."}
-    )
     model_name_or_path: str = field(
-        metadata={"help": "Path to pretrained model or model identifier from huggingface.co/models"}
+        metadata={
+            "help": "Path to pretrained model or model identifier from huggingface.co/models"}
     )
     config_name: Optional[str] = field(
-        default=None, 
-        metadata={"help": "Pretrained config name or path if not the same as model_name"}
+        default=None,
+        metadata={
+            "help": "Pretrained config name or path if not the same as model_name"}
     )
     tokenizer_name: Optional[str] = field(
-        default=None, 
-        metadata={"help": "Pretrained tokenizer name or path if not the same as model_name"}
+        default=None,
+        metadata={
+            "help": "Pretrained tokenizer name or path if not the same as model_name"}
     )
     use_fast: bool = field(
-        default=False, 
+        default=False,
         metadata={"help": "Set this flag to use fast tokenization."}
     )
     # If you want to tweak more attributes on your tokenizer, you should do it in a distinct script,
     # or just modify its tokenizer_config.json.
     cache_dir: Optional[str] = field(
-        default=None, 
-        metadata={"help": "Where do you want to store the pretrained models downloaded from s3"}
+        default=None,
+        metadata={
+            "help": "Where do you want to store the pretrained models downloaded from s3"}
     )
