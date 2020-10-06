a5693f60e0f01a134aad815411cf487d9522c9e7
Yu-Lun Chiang <chiangyulun0914@gmail.com>
2020-10-06 14:38:35 +0800

    init BertSLModel

---
+++ models/bert_sl.py
@@ -0,0 +1,60 @@
+# encoding=utf-8
+# Author: Allen.Chiang
+# Description: bert model for sequence labeling
+
+import logging
+import torch
+from torch.nn import Linear, Dropout, CrossEntropyLoss
+from transformers import BertConfig, BertPreTrainedModel, BertModel
+from utils.modeling_output import TokenClassificationModelOutput
+
+logger = logging.getLogger(__name__)
+
+
+class BertSLModel(BertPreTrainedModel):
+
+    def __init__(self, config):
+        super().__init__(config)
+        self.num_labels = config.num_labels
+        self.bert = BertModel(config, add_pooling_layer=False)
+        self.dropout = Dropout(config.hidden_dropout_prob)
+        self.classifier = Linear(config.hidden_size, config.num_labels)
+        self.init_weights()
+
+    def forward(
+        self,
+        input_ids=None,
+        token_type_ids=None,
+        attention_mask=None,
+        labels=None
+    ):
+        outputs = self.bert(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids
+        )
+
+        sequence_output = outputs[0]
+        sequence_output = self.dropout(sequence_output)
+        logits = self.classifier(sequence_output)
+
+        loss = None
+        if labels is not None:
+            loss_fct = CrossEntropyLoss()
+            # Only keep active parts of the loss
+            if attention_mask is not None:
+                active_loss = attention_mask.view(-1) == 1
+                active_logits = logits.view(-1, self.num_labels)
+                active_labels = torch.where(
+                    active_loss, labels.view(-1), torch.tensor(loss_fct.ignore_index).type_as(labels)
+                )
+                loss = loss_fct(active_logits, active_labels)
+            else:
+                loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+
+        return TokenClassificationModelOutput(
+            loss=loss,
+            logits=logits,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )
