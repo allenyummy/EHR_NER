922fe3fc412694ad1e8b671963c920309d56daf9
Yu-Lun Chiang <chiangyulun0914@gmail.com>
2020-10-06 14:39:33 +0800

    add model output

--- models/bert_mrc.py
+++ models/bert_mrc.py
@@ -5,6 +5,7 @@
 import logging
 from torch.nn import Linear, CrossEntropyLoss
 from transformers import BertConfig, BertPreTrainedModel, BertModel
+from utils.modeling_output import QuestionAnsweringModelOutput
 
 logger = logging.getLogger(__name__)
 
@@ -13,12 +14,9 @@ class BertMRCModel(BertPreTrainedModel):
 
     def __init__(self, config):
         super().__init__(config)
-
         self.bert = BertModel(config, add_pooling_layer=False)
-
         self.start_outputs = Linear(config.hidden_size, 2)
         self.end_outoputs = Linear(config.hidden_size, 2)
-
         self.init_weights()
 
     def forward(
@@ -38,14 +36,18 @@ class BertMRCModel(BertPreTrainedModel):
         sequence_output = outputs[0]
         start_logits = self.start_outputs(sequence_output)
         end_logits = self.end_outoputs(sequence_output)
-        outputs = (start_logits, end_logits) + outputs[2:]
 
-        if start_logits is not None and end_logits is not None:
+        loss = None
+        if start_positions is not None and end_positions is not None:
             loss_fct = CrossEntropyLoss()
-
             start_loss = loss_fct(start_logits.view(-1, 2), start_positions.view(-1))
             end_loss = loss_fct(end_logits.view(-1, 2), end_positions.view(-1))
             loss = (start_loss + end_loss) / 2
 
-        outputs = (loss,) + outputs
-        return outputs
+        return QuestionAnsweringModelOutput(
+            loss=loss,
+            start_logits=start_logits,
+            end_logits=end_logits,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )
