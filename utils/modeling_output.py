9651c5e2b928f0ebf92dd889bf8e4e932c79bef9
Yu-Lun Chiang <chiangyulun0914@gmail.com>
2020-10-07 10:04:52 +0800

    inherit ModelOutput to let trainer work fine

--- utils/modeling_output.py
+++ utils/modeling_output.py
@@ -6,19 +6,20 @@ import logging
 import torch
 from typing import Optional, Tuple
 from dataclasses import dataclass
+from transformers.file_utils import ModelOutput
 
 logger = logging.getLogger(__name__)
 
 
 @dataclass
-class TokenClassificationModelOutput:
+class TokenClassificationModelOutput(ModelOutput):
     loss: Optional[torch.FloatTensor] = None
     logits: torch.FloatTensor = None
     hidden_states: Optional[Tuple[torch.FloatTensor]] = None
     attentions: Optional[Tuple[torch.FloatTensor]] = None
 
 @dataclass
-class QuestionAnsweringModelOutput:
+class QuestionAnsweringModelOutput(ModelOutput):
     loss: Optional[torch.FloatTensor] = None
     start_logits: torch.FloatTensor = None
     end_logits: torch.FloatTensor = None
