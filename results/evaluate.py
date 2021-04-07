2265be6c919854d879f2451db6222d177268c9bd
Yu-Lun Chiang <chiangyulun0914@gmail.com>
2021-04-07 16:17:30 +0800

    [modified]

--- results/evaluate.py
+++ results/evaluate.py
@@ -9,6 +9,7 @@ from api.bert_qasl_predictor import BertQASLPredictor
 
 df = pd.DataFrame(
     columns=[
+        "pid",
         "passage",
         "flat_ne_answers",
         "nested_ne_answers",
@@ -25,7 +26,7 @@ data_path = os.path.join(
 query_path = os.path.join(
     "data", "final", "query", "simqasl_query.json"
 )
-out_data_path = os.path.join("results", "overall_dev_checked.xlsx")
+out_data_path = os.path.join("results", "nestedner_dev.xlsx")
 
 with open(query_path, "r", encoding="utf-8") as fq:
     query = json.load(fq)
@@ -35,28 +36,38 @@ with open(data_path, "r", encoding="utf-8") as f:
 data = in_data["data"]
 
 Bert_SL_model = BertSLPredictor(
-    model_dir="trained_model/0817_8786_concat_num/sl/2020-12-10-05@hfl@chinese-bert-wwm@CE_S-512_B-8_E-20_LR-5e-5_SD-1",
+    model_dir="exp/0817_8786_concat_num/sl/2021-01-12-05-V3.0-flatner@hfl@chinese-bert-wwm@CE_S-512_B-8_E-40_LR-5e-5_SD-1_load-best/",
     with_bilstmcrf=False,
 )
 BertBiLSTMCRF_SL_model = BertSLPredictor(
-    model_dir="trained_model/0817_8786_concat_num/sl/2020-12-14-09@hfl@chinese-bert-wwm@BiLSTMCRF_S-512_B-8_E-20_LR-5e-5_SD-1",
+    model_dir="exp/0817_8786_concat_num/sl/2021-01-12-06-V3.0-flatner@hfl@chinese-bert-wwm@BiLSTMCRF_S-512_B-8_E-40_LR-5e-5_SD-1_load-best",
     with_bilstmcrf=True,
 )
+# Bert_QASL_model = BertQASLPredictor(
+#     model_dir="exp/0817_8786_concat_num/simqasl/2021-01-12-09-V3.0-flatner@hfl@chinese-bert-wwm@wCE-0.11-1-0.16_S-512_B-8_E-40_LR-5e-5_SD-1_load-best",
+#     with_bilstmcrf=False,
+# )
+# BertBiLSTMCRF_QASL_model = BertQASLPredictor(
+#    model_dir="exp/0817_8786_concat_num/simqasl/2021-01-21-03-V3.0-flatner@hfl@chinese-bert-wwm@wBiLSTMCRF-0.11-1-0.16_S-512_B-8_E-40_LR-5e-5_SD-1_load-best",
+#    with_bilstmcrf=True,
+# )
 Bert_QASL_model = BertQASLPredictor(
-    model_dir="trained_model/0817_8786_concat_num/simqasl/2020-12-10-07@hfl@chinese-bert-wwm@weightedCE-0.11-1-0.16_S-512_B-8_E-20_LR-5e-5_SD-1",
+    model_dir="exp/0817_8786_concat_num/simqasl/2021-01-14-01-V3.0-nestedner@hfl@chinese-bert-wwm@wCE-0.11-1-0.16_S-512_B-8_E-40_LR-5e-5_SD-1_load-best",
     with_bilstmcrf=False,
 )
 BertBiLSTMCRF_QASL_model = BertQASLPredictor(
-    model_dir="trained_model/0817_8786_concat_num/simqasl/2020-12-17-07@hfl@chinese-bert-wwm@wBiLSTMCRF-0.11-1-0.16_S-512_B-8_E-20_LR-5e-5_SD-1",
-    with_bilstmcrf=True,
+   model_dir="exp/0817_8786_concat_num/simqasl/2021-01-18-01-V3.0-nestedner@hfl@chinese-bert-wwm@wBiLSTMCRF-0.11-1-0.16_S-512_B-8_E-40_LR-5e-5_SD-1_load-best/checkpoint-110000",
+   with_bilstmcrf=True,
 )
 
 
-for i, d in enumerate(tqdm(data)):
 
+for i, d in enumerate(tqdm(data)):
+    
     pid = d["pid"]
     passage = d["passage"]
     passage_tokens = d["passage_tokens"]
+    
     input_passage = " ".join(passage_tokens)
     flat_ne_answers = d["flat_ne_answers"]
     nested_ne_answers = d["nested_ne_answers"]
@@ -65,12 +76,9 @@ for i, d in enumerate(tqdm(data)):
     flat_ne_answers_results = list()
     flat_ne_answers_set = set()
     for r in flat_ne_answers:
-        if r["type"] == "DIN":   ## QASL doesn't handle this label
-            pass
-        else:
-            o = f"[{r['type']}][{r['text']}][{r['start_pos']}-{r['end_pos']}]"
-            flat_ne_answers_results.append(o)
-            flat_ne_answers_set.add((r["type"], r["text"], r["start_pos"], r["end_pos"]))
+        o = f"[{r['type']}][{r['text']}][{r['start_pos']}-{r['end_pos']}]"
+        flat_ne_answers_results.append(o)
+        flat_ne_answers_set.add((r["type"], r["start_pos"], r["end_pos"]))
     flat_ne_answers_results = ",\n".join(flat_ne_answers_results)
             
     # --- Generate nested ne ansewers ---
@@ -79,7 +87,7 @@ for i, d in enumerate(tqdm(data)):
     for r in nested_ne_answers:
         o = f"[{r['type']}][{r['text']}][{r['start_pos']}-{r['end_pos']}]"
         nested_ne_answers_results.append(o)
-        nested_ne_answers_set.add((r["type"], r["text"], r["start_pos"], r["end_pos"]))
+        nested_ne_answers_set.add((r["type"], r["start_pos"], r["end_pos"]))
     nested_ne_answers_results = ",\n".join(nested_ne_answers_results)
 
     # --- Predict by Bert_SL_model ---
@@ -92,7 +100,7 @@ for i, d in enumerate(tqdm(data)):
     for r in bert_sl_pred_entities:
         o = f"[{r.type}][{r.text}][{r.start_pos}-{r.end_pos}]"
         bert_sl_pred_results.append(o)
-        bert_sl_pred_entities_set.add((r.type, r.text, r.start_pos, r.end_pos))
+        bert_sl_pred_entities_set.add((r.type, r.start_pos, r.end_pos))
     bert_sl_pred_results = ",\n".join(bert_sl_pred_results)
     
 
@@ -106,7 +114,7 @@ for i, d in enumerate(tqdm(data)):
     for r in bertbilstmcrf_sl_pred_entities:
         o = f"[{r.type}][{r.text}][{r.start_pos}-{r.end_pos}]"
         bertbilstmcrf_sl_pred_results.append(o)
-        bertbilstmcrf_sl_pred_entities_set.add((r.type, r.text, r.start_pos, r.end_pos))
+        bertbilstmcrf_sl_pred_entities_set.add((r.type, r.start_pos, r.end_pos))
     bertbilstmcrf_sl_pred_results = ",\n".join(bertbilstmcrf_sl_pred_results)
 
     # --- Predict by Bert_QASL_model ---
@@ -121,7 +129,7 @@ for i, d in enumerate(tqdm(data)):
     for r in sorted(bert_qasl_pred_entities, key=lambda x: x.start_pos):
         o = f"[{r.type}][{r.text}][{r.start_pos}-{r.end_pos}]"
         bert_qasl_pred_results.append(o)
-        bert_qasl_pred_entities_set.add((r.type, r.text, r.start_pos, r.end_pos))
+        bert_qasl_pred_entities_set.add((r.type, r.start_pos, r.end_pos))
     bert_qasl_pred_results = ",\n".join(bert_qasl_pred_results)
 
     # --- Predict by BertBiLSTMCRF_QASL_model ---
@@ -136,17 +144,18 @@ for i, d in enumerate(tqdm(data)):
     for r in sorted(bertbilstmcrf_qasl_pred_entities, key=lambda x: x.start_pos):
         o = f"[{r.type}][{r.text}][{r.start_pos}-{r.end_pos}]"
         bertbilstmcrf_qasl_pred_results.append(o)
-        bertbilstmcrf_qasl_pred_entities_set.add((r.type, r.text, r.start_pos, r.end_pos))
+        bertbilstmcrf_qasl_pred_entities_set.add((r.type, r.start_pos, r.end_pos))
     bertbilstmcrf_qasl_pred_results = ",\n".join(bertbilstmcrf_qasl_pred_results)
 
     # --- Overall Result ---
+    df.loc[pid, "pid"] = pid
     df.loc[pid, "passage"] = passage
     df.loc[pid, "flat_ne_answers"] = flat_ne_answers_results
     df.loc[pid, "nested_ne_answers"] = nested_ne_answers_results
     df.loc[pid, "bert_sl_pred"] = bert_sl_pred_results
     df.loc[pid, "bertbilstmcrf_sl_pred"] = bertbilstmcrf_sl_pred_results
-    # df.loc[pid, "bert_qasl_pred"] = bert_qasl_pred_results
-    # df.loc[pid, "bertbilstmcrf_qasl_pred"] = bertbilstmcrf_qasl_pred_results
+    df.loc[pid, "bert_qasl_pred"] = bert_qasl_pred_results
+    df.loc[pid, "bertbilstmcrf_qasl_pred"] = bertbilstmcrf_qasl_pred_results
 
     df.loc[pid, "flatne_tn"] = len(flat_ne_answers_set)
     df.loc[pid, "nestedne_tn"] = len(nested_ne_answers_set)
